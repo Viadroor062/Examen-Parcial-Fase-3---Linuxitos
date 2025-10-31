15f189f71619c38b0cebaa4590481871f8f16deb
Viadroor062 <victor.rodriguezotz@uanl.edu.mx>
2025-10-30 21:40:56 -0600

    Delete coords_helper.py

--- coords_helper.py
+++
@@ -1,9 +0,0 @@
-# coords_helper.py
-import pyautogui
-import time
-
-print("Mueve el cursor a la posición deseada...")
-time.sleep(10)
-pos = pyautogui.position()
-print(f"Coordenadas actuales: {pos}")
-
